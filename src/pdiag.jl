7fb0f5ae5ec8b8d28d99ec175be6a8e2e84ecf28
wildart <wildart@gmail.com>
2019-01-15 01:53:50 -0500

    rewrote for intervals dict

--- src/pdiag.jl
+++ src/pdiag.jl
@@ -1,31 +1,35 @@
 # Plot intervals as a persistance diagram or barcode
-@recipe function f(ints::Vector{Vector{T}}; skipzero = false) where {T<:Interval}
+@recipe function f(ints::Dict{Int, Vector{Interval}};
+        maxoutdim=length(ints),
+        skipzero = false)
+    # set default plot type
     seriestype --> :diagram
 
-    maxval = mapreduce(l->maximum(filter(!isinf, map(i->i.d,l))), max, ints)
-    minval = mapreduce(l->minimum(filter(!isinf, map(i->i.b,l))), min, ints)
+    vals = vcat(([i.b, i.d] for i in vcat(values(ints)...))...)
+    minval, maxval = extrema(filter!(!isinf, vals))
     #col = vcat(map(l->map(i->isinf(i.d) ? :red : :auto ,l), ints)...)
+    dims = [d for d in sort!(filter!(d->d<=maxoutdim, collect(keys(ints))))]
 
     if plotattributes[:seriestype] == :barcode # Persistance Barcode
-        xticks := minval:maxval
-        xlims  := (minval, maxval+0.05)
+        #xticks := minval:maxval
+        xlims  := (minval, maxval*1.01)
         yticks := nothing
         ylims := (0.0, 1.05)
         legend := :none
-        layout := (length(ints),1)
-        for d in 1:length(ints)
+        layout := (length(dims),1)
+        for (pi, d) in enumerate(dims)
+            dmaxval = max(maxval, foldl(max, filter(!isinf, map(i->i.d, ints[d])); init=-Inf))
+            dminval = min(minval, foldl(min, filter(!isinf, map(i->i.b, ints[d])); init=Inf))
             points = map(i -> i.b == i.d, ints[d])
             step = 1 / (skipzero ? sum(.!points) : length(ints[d]))
             y = step
             for (i, ispoint) in zip(ints[d], points)
                 xs, ys = [i.b, isinf(i.d) ? maxval : i.d], [y, y]
-                println("ispoint: $ispoint")
-                println(!ispoint && skipzero)
                 ispoint && skipzero && continue
                 @series begin
-                    title := "Degree $(d-1)"
+                    title := "Degree $d"
                     linewidth --> 2
-                    subplot := d
+                    subplot := pi
                     primary := true
                     arrow := isinf(i.d)
                     seriescolor --> :blue
@@ -37,18 +41,20 @@
             end
         end
     else # Persistance Diagram
-        xs = map(l->map(i->i.b,l), ints)
-        ys = map(l->map(i->isinf(i.d) ? maxval : i.d,l), ints)
-        mkr = vcat(map(l->map(i->isinf(i.d) ? :rect : :circle,l), ints)...)
+        padding = (maxval - minval)*0.01 # 3%
+        xs = map(l->map(i->i.b,l), ints[d] for d in dims)
+        ys = map(l->map(i->isinf(i.d) ? maxval : i.d,l), ints[d] for d in dims)
+        mkr = map(l->map(i->isinf(i.d) ? :rect : :circle,l), ints[d] for d in dims)
         legend --> :bottomright
         legendtitle --> "Homology"
         label --> ["Degree $(i-1)" for i in 1:length(ints)]
-        xticks := minval:maxval
-        xlims := (minval-0.3, maxval+0.3)
+        #xticks := minval:maxval
+        xlims := (minval-padding, maxval+padding)
         xlabel := "birth"
-        yticks := 0:maxval
+        #yticks := 0:maxval
         ylabel := "death"
-        ylims := (minval, maxval+0.3)
+        ylims := (minval, maxval*1.03)
+
         @series begin
             primary := false
             seriestype := :path
@@ -56,9 +62,16 @@
             [0, maxval], [0, maxval]
         end
 
-        seriestype := :scatter
-        markershape := mkr
-        #markerstrokecolor := col
-        xs, ys
+        for i in 1:length(xs)
+            idxs = skipzero ? xs[i] .!== ys[i] : .!BitArray(undef, length(xs[i]))
+            @series begin
+                primary := true
+                seriestype := :scatter
+                markershape := mkr[i][idxs]
+                markersize --> 2
+                #markerstrokecolor := col
+                xs[i][idxs], ys[i][idxs]
+            end
+        end
     end
 end
